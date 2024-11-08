9481e91676e6be527a99c5615cf176246d8b0d2c
Jorge Calero <jcalero@dabbleam.com>
2024-11-09 00:53:45 +0100

    web cv

--- scripts.js
+++ scripts.js
@@ -254,4 +254,3 @@ const translations = {
         ? "https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg"
         : "https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Spain.svg";
   }
-  
