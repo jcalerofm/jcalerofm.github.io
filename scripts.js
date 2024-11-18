9f36b085057b51d0f3cee1a67d572dc45caab865
Jorge Calero <jcalero@dabbleam.com>
2024-11-18 22:41:08 +0100

    web cv

--- scripts.js
+++ scripts.js
@@ -1,266 +1,267 @@
 const translations = {
-    "language-text": {
-      es: "English",
-      en: "Español"
-    },
-    title: {
-      es: "Full-Stack Developer",
-      en: "Full-Stack Developer"
-    },
-    "perfil-education": {
-      es: "Técnico Desarrollo de Software",
-      en: "Software Development Technician"
-    },
-    "perfil-degree": {
-      es: "Grado en Economía y Empresas turísticas",
-      en: "Degree in Economics and Tourism"
-    },
-    "contact-title": {
-      es: "Contacto",
-      en: "Contact"
-    },
-    "linkedin-text": {
-      es: "LinkedIn",
-      en: "LinkedIn"
-    },
-    "languages-title": {
-      es: "Idiomas",
-      en: "Languages"
-    },
-    "language-spanish": {
-      es: "Español",
-      en: "Spanish"
-    },
-    "nativo": {
-      es: "Nativo",
-      en: "Native"
-    },
-    "language-english": {
-      es: "Inglés",
-      en: "English"
-    },
-    "language-french": {
-      es: "Francés",
-      en: "French"
-    },
-    "language-other": {
-      es: "Otros",
-      en: "Others"
-    },
-    "profile-title": {
-      es: "Perfil",
-      en: "Profile"
-    },
-    "perfil-text": {
-      es: "Desarrollador Full Stack con un perfil técnico sólido y una pasión por crear soluciones seguras, eficientes y escalables. <b>Técnico Superior en Desarrollo de Software</b> con un conjunto de habilidades que incluyen programación web, diseño de interfaces intuitivas, y administración de bases de datos. Además de mi experiencia técnica, mis 14 años en atención al cliente en el sector turístico me han brindado una comprensión única de la experiencia del usuario y habilidades de comunicación y resolución de problemas. Estoy en busca de un rol donde pueda combinar mi creatividad y conocimientos técnicos, y continuar desarrollándome en proyectos innovadores que aporten valor real a las empresas y a sus clientes.",
-      en: "Full Stack Developer with a solid technical background and a passion for creating secure, efficient, and scalable solutions. <b>Higher Technician in Software Development</b> with a skill set including web programming, intuitive interface design, and database management. In addition to my technical experience, my 14 years in customer service in the tourism sector have given me a unique understanding of user experience and excellent communication and problem-solving skills. I am looking for a role where I can combine my creativity and technical knowledge and continue developing innovative projects that add real value to companies and their clients."
-    },
-    "experience-title": {
-      es: "Experiencia",
-      en: "Experience"
-    },
-    "experience-company-1": {
-      es: "Dabbleam",
-      en: "Dabbleam"
-    },
-    "experience-date-1": {
-      es: "Abril 2022 - Actualidad",
-      en: "April 2022 - Present"
-    },
-    "dabbleam-text": {
-      es: "Análisis, diseño y soluciones tecnológicas para todo tipo de usuarios. Tanto particulares como empresas. Desarrollos internos como Porteria.de y nowUtoo.",
-      en: "Analysis, design, and technological solutions for all types of users. Both individuals and companies. Internal developments such as Porteria.de and nowUtoo."
-    },
-    "experience-company-2": {
-      es: "Trigo Aerospace/Airbus",
-      en: "Trigo Aerospace/Airbus"
-    },
-    "experience-date-2": {
-      es: "Febrero 2022 - Abril 2022",
-      en: "February 2022 - April 2022"
-    },
-    "trigo-text": {
-      es: "Análisis de datos y desarrollo de aplicaciones para su plataforma Big Data para toda la industria de la aviación SKYWISE (Python, SQL, Javascript, HTML, CSS, Bootstrap).",
-      en: "Data analysis and application development for their Big Data platform for the entire aviation industry SKYWISE (Python, SQL, Javascript, HTML, CSS, Bootstrap)."
-    },
-    "experience-company-3": {
-      es: "Le Wagon (Berlin)",
-      en: "Le Wagon (Berlin)"
-    },
-    "experience-date-3": {
-      es: "Septiembre 2019 - Octubre 2021",
-      en: "September 2019 - October 2021"
-    },
-    "le-wagon-text": {
-      es: "De asistente de profesor a profesor a tiempo completo. Enseñar Ruby on Rails y el resto de tecnología que se imparte en Le Wagon. Seguimiento de la evolución y aprendizaje de los alumnos.",
-      en: "From assistant teacher to full-time teacher. Teaching Ruby on Rails and other technologies taught at Le Wagon. Monitoring student progress and learning."
-    },
-    "experience-company-4": {
-      es: "Enterprise Holdings",
-      en: "Enterprise Holdings"
-    },
-    "experience-date-4": {
-      es: "Julio 2018 - Diciembre 2018",
-      en: "July 2018 - December 2018"
-    },
-    "enterprise-text": {
-      es: "Trabajo de ventas por objetivos. Supervisión de las normas de calidad. Resolución de incidencias. Introducción de datos en sistema.",
-      en: "Sales work based on objectives. Supervision of quality standards. Issue resolution. Data entry into the system."
-    },
-    "experience-company-5": {
-      es: "Viajes El Corte Inglés",
-      en: "Viajes El Corte Inglés"
-    },
-    "experience-date-5": {
-      es: "Septiembre 2005 - Junio 2018",
-      en: "September 2005 - June 2018"
-    },
-    "viajes-text": {
-      es: "Durante 2010-2012: Etapa en América Latina. Desarrollo de apertura de oficinas en México y Colombia. Trabajo orientado al posicionamiento de la marca en nuevos mercados.",
-      en: "During 2010-2012: Period in Latin America. Office openings in Mexico and Colombia. Work focused on brand positioning in new markets."
-    },
-    "education-title": {
-      es: "Educación",
-      en: "Education"
-    },
-    "educacion-item-1": {
-      es: "Técnico Superior en Desarrollo de Software",
-      en: "Higher Technician in Software Development"
-    },
-    "education-date-1": {
-      es: "2022-2023",
-      en: "2022-2023"
-    },
-    "educacion-item-2": {
-      es: "Le Wagon (Bootcamp de desarrollo)",
-      en: "Le Wagon (Development Bootcamp)"
-    },
-    "education-date-2": {
-      es: "Julio 2019 - Septiembre 2019",
-      en: "July 2019 - September 2019"
-    },
-    "educacion-item-3": {
-      es: "Grado en Economía y Turismo",
-      en: "Degree in Economics and Tourism"
-    },
-    "education-date-3": {
-      es: "2004-2008",
-      en: "2004-2008"
-    },
-    "skills-title": {
-      es: "Habilidades",
-      en: "Skills"
-    },
-    "backend-title": {
-      es: "Backend",
-      en: "Backend"
-    },
-    "frontend-title": {
-      es: "Frontend",
-      en: "Frontend"
-    },
-    "habilidad-item-1": {
-      es: "Ruby on Rails/Laravel",
-      en: "Ruby on Rails/Laravel"
-    },
-    "habilidad-item-2": {
-      es: "Angular/React",
-      en: "Angular/React"
-    },
-    "habilidad-item-3": {
-      es: "API REST",
-      en: "REST API"
-    },
-    "habilidad-item-4": {
-      es: "SQL, MySQL, PostgreSQL",
-      en: "SQL, MySQL, PostgreSQL"
-    },
-    "habilidad-item-5": {
-      es: "ES6 Javascript",
-      en: "ES6 Javascript"
-    },
-    "habilidad-item-6": {
-      es: "Python",
-      en: "Python"
-    },
-    "habilidad-item-7": {
-      es: "Arquitectura de microservicios",
-      en: "Microservices Architecture"
-    },
-    "habilidad-item-8": {
-      es: "HTML/CSS/Bootstrap",
-      en: "HTML/CSS/Bootstrap"
-    },
-    "habilidad-item-9": {
-      es: "Docker",
-      en: "Docker"
-    },
-    "habilidad-item-11": {
-      es: "Git, control de versiones",
-      en: "Git, technical workflow"
-    },
-    "habilidad-item-12": {
-      es: "WordPress",
-      en: "WordPress"
-    },
-    "quick-contact-title": {
-      es: "Contacto Rápido",
-      en: "Quick Contact"
-    },
-    "name-label": {
-      es: "Nombre:",
-      en: "Name:"
-    },
-    "email-label": {
-      es: "Correo Electrónico:",
-      en: "Email:"
-    },
-    "message-label": {
-      es: "Mensaje:",
-      en: "Message:"
-    },
-    "send-button": {
-      es: "Enviar Mensaje",
-      en: "Send Message"
-    },
-    "form-response-success": {
-      es: "Gracias, tu mensaje ha sido enviado.",
-      en: "Thank you, your message has been sent."
-    },
-    "form-response-error": {
-      es: "Lo sentimos, hubo un error al enviar tu mensaje.",
-      en: "Sorry, there was an error sending your message."
-    },
-    "form-response-honeypot": {
-      es: "Se detectó actividad de bot.",
-      en: "Bot activity detected."
-    },
-    "form-response-validation": {
-      es: "Por favor completa todos los campos correctamente y vuelve a intentarlo.",
-      en: "Please complete all fields correctly and try again."
-    }
-  };
-  
-  function toggleLanguage() {
-    const currentLang = document.documentElement.lang;
-    const newLang = currentLang === "es" ? "en" : "es";
-    document.documentElement.lang = newLang;
-  
-    for (const [id, texts] of Object.entries(translations)) {
-      const element = document.getElementById(id);
-  
-      if (element) {
-        if (element.querySelector(".icon")) {
-          element.querySelector(".icon").nextSibling.nodeValue = ` ${texts[newLang]}`;
-        } else {
-          element.innerHTML = texts[newLang];
-        }
+  "language-text": {
+    es: "English",
+    en: "Español",
+  },
+  title: {
+    es: "Full-Stack Developer",
+    en: "Full-Stack Developer",
+  },
+  "perfil-education": {
+    es: "Técnico Desarrollo de Software",
+    en: "Software Development Technician",
+  },
+  "perfil-degree": {
+    es: "Grado en Economía y Empresas turísticas",
+    en: "Degree in Economics and Tourism",
+  },
+  "contact-title": {
+    es: "Contacto",
+    en: "Contact",
+  },
+  "linkedin-text": {
+    es: "LinkedIn",
+    en: "LinkedIn",
+  },
+  "languages-title": {
+    es: "Idiomas",
+    en: "Languages",
+  },
+  "language-spanish": {
+    es: "Español",
+    en: "Spanish",
+  },
+  nativo: {
+    es: "Nativo",
+    en: "Native",
+  },
+  "language-english": {
+    es: "Inglés",
+    en: "English",
+  },
+  "language-french": {
+    es: "Francés",
+    en: "French",
+  },
+  "language-other": {
+    es: "Otros",
+    en: "Others",
+  },
+  "profile-title": {
+    es: "Perfil",
+    en: "Profile",
+  },
+  "perfil-text": {
+    es: "Desarrollador Full Stack con un perfil técnico sólido y una pasión por crear soluciones seguras, eficientes y escalables. <b>Técnico Superior en Desarrollo de Software</b> con un conjunto de habilidades que incluyen programación web, diseño de interfaces intuitivas, y administración de bases de datos. Además de mi experiencia técnica, mis 14 años en atención al cliente en el sector turístico me han brindado una comprensión única de la experiencia del usuario y habilidades de comunicación y resolución de problemas. Estoy en busca de un rol donde pueda combinar mi creatividad y conocimientos técnicos, y continuar desarrollándome en proyectos innovadores que aporten valor real a las empresas y a sus clientes.",
+    en: "Full Stack Developer with a solid technical background and a passion for creating secure, efficient, and scalable solutions. <b>Higher Technician in Software Development</b> with a skill set including web programming, intuitive interface design, and database management. In addition to my technical experience, my 14 years in customer service in the tourism sector have given me a unique understanding of user experience and excellent communication and problem-solving skills. I am looking for a role where I can combine my creativity and technical knowledge and continue developing innovative projects that add real value to companies and their clients.",
+  },
+  "experience-title": {
+    es: "Experiencia",
+    en: "Experience",
+  },
+  "experience-company-1": {
+    es: "Dabbleam",
+    en: "Dabbleam",
+  },
+  "experience-date-1": {
+    es: "Abril 2022 - Actualidad",
+    en: "April 2022 - Present",
+  },
+  "dabbleam-text": {
+    es: "Análisis, diseño y soluciones tecnológicas para todo tipo de usuarios. Tanto particulares como empresas. Desarrollos internos como Porteria.de y nowUtoo.",
+    en: "Analysis, design, and technological solutions for all types of users. Both individuals and companies. Internal developments such as Porteria.de and nowUtoo.",
+  },
+  "experience-company-2": {
+    es: "Trigo Aerospace/Airbus",
+    en: "Trigo Aerospace/Airbus",
+  },
+  "experience-date-2": {
+    es: "Febrero 2022 - Abril 2022",
+    en: "February 2022 - April 2022",
+  },
+  "trigo-text": {
+    es: "Análisis de datos y desarrollo de aplicaciones para su plataforma Big Data para toda la industria de la aviación SKYWISE (Python, SQL, Javascript, HTML, CSS, Bootstrap).",
+    en: "Data analysis and application development for their Big Data platform for the entire aviation industry SKYWISE (Python, SQL, Javascript, HTML, CSS, Bootstrap).",
+  },
+  "experience-company-3": {
+    es: "Le Wagon (Berlin)",
+    en: "Le Wagon (Berlin)",
+  },
+  "experience-date-3": {
+    es: "Septiembre 2019 - Octubre 2021",
+    en: "September 2019 - October 2021",
+  },
+  "le-wagon-text": {
+    es: "De asistente de profesor a profesor a tiempo completo. Enseñar Ruby on Rails y el resto de tecnología que se imparte en Le Wagon. Seguimiento de la evolución y aprendizaje de los alumnos.",
+    en: "From assistant teacher to full-time teacher. Teaching Ruby on Rails and other technologies taught at Le Wagon. Monitoring student progress and learning.",
+  },
+  "experience-company-4": {
+    es: "Enterprise Holdings",
+    en: "Enterprise Holdings",
+  },
+  "experience-date-4": {
+    es: "Julio 2018 - Diciembre 2018",
+    en: "July 2018 - December 2018",
+  },
+  "enterprise-text": {
+    es: "Trabajo de ventas por objetivos. Supervisión de las normas de calidad. Resolución de incidencias. Introducción de datos en sistema.",
+    en: "Sales work based on objectives. Supervision of quality standards. Issue resolution. Data entry into the system.",
+  },
+  "experience-company-5": {
+    es: "Viajes El Corte Inglés",
+    en: "Viajes El Corte Inglés",
+  },
+  "experience-date-5": {
+    es: "Septiembre 2005 - Junio 2018",
+    en: "September 2005 - June 2018",
+  },
+  "viajes-text": {
+    es: "Durante 2010-2012: Etapa en América Latina. Desarrollo de apertura de oficinas en México y Colombia. Trabajo orientado al posicionamiento de la marca en nuevos mercados.",
+    en: "During 2010-2012: Period in Latin America. Office openings in Mexico and Colombia. Work focused on brand positioning in new markets.",
+  },
+  "education-title": {
+    es: "Educación",
+    en: "Education",
+  },
+  "educacion-item-1": {
+    es: "Técnico Superior en Desarrollo de Software",
+    en: "Higher Technician in Software Development",
+  },
+  "education-date-1": {
+    es: "2022-2023",
+    en: "2022-2023",
+  },
+  "educacion-item-2": {
+    es: "Le Wagon (Bootcamp de desarrollo)",
+    en: "Le Wagon (Development Bootcamp)",
+  },
+  "education-date-2": {
+    es: "Julio 2019 - Septiembre 2019",
+    en: "July 2019 - September 2019",
+  },
+  "educacion-item-3": {
+    es: "Grado en Economía y Turismo",
+    en: "Degree in Economics and Tourism",
+  },
+  "education-date-3": {
+    es: "2004-2008",
+    en: "2004-2008",
+  },
+  "skills-title": {
+    es: "Habilidades",
+    en: "Skills",
+  },
+  "backend-title": {
+    es: "Backend",
+    en: "Backend",
+  },
+  "frontend-title": {
+    es: "Frontend",
+    en: "Frontend",
+  },
+  "habilidad-item-1": {
+    es: "Ruby on Rails/Laravel",
+    en: "Ruby on Rails/Laravel",
+  },
+  "habilidad-item-2": {
+    es: "Angular/React",
+    en: "Angular/React",
+  },
+  "habilidad-item-3": {
+    es: "API REST",
+    en: "REST API",
+  },
+  "habilidad-item-4": {
+    es: "SQL, MySQL, PostgreSQL",
+    en: "SQL, MySQL, PostgreSQL",
+  },
+  "habilidad-item-5": {
+    es: "ES6 Javascript",
+    en: "ES6 Javascript",
+  },
+  "habilidad-item-6": {
+    es: "Python",
+    en: "Python",
+  },
+  "habilidad-item-7": {
+    es: "Arquitectura de microservicios",
+    en: "Microservices Architecture",
+  },
+  "habilidad-item-8": {
+    es: "HTML/CSS/Bootstrap",
+    en: "HTML/CSS/Bootstrap",
+  },
+  "habilidad-item-9": {
+    es: "Docker",
+    en: "Docker",
+  },
+  "habilidad-item-11": {
+    es: "Git, control de versiones",
+    en: "Git, technical workflow",
+  },
+  "habilidad-item-12": {
+    es: "WordPress",
+    en: "WordPress",
+  },
+  "quick-contact-title": {
+    es: "Contacto Rápido",
+    en: "Quick Contact",
+  },
+  "name-label": {
+    es: "Nombre:",
+    en: "Name:",
+  },
+  "email-label": {
+    es: "Correo Electrónico:",
+    en: "Email:",
+  },
+  "message-label": {
+    es: "Mensaje:",
+    en: "Message:",
+  },
+  "send-button": {
+    es: "Enviar Mensaje",
+    en: "Send Message",
+  },
+  "form-response-success": {
+    es: "Gracias, tu mensaje ha sido enviado.",
+    en: "Thank you, your message has been sent.",
+  },
+  "form-response-error": {
+    es: "Lo sentimos, hubo un error al enviar tu mensaje.",
+    en: "Sorry, there was an error sending your message.",
+  },
+  "form-response-honeypot": {
+    es: "Se detectó actividad de bot.",
+    en: "Bot activity detected.",
+  },
+  "form-response-validation": {
+    es: "Por favor completa todos los campos correctamente y vuelve a intentarlo.",
+    en: "Please complete all fields correctly and try again.",
+  },
+};
+
+function toggleLanguage() {
+  const currentLang = document.documentElement.lang;
+  const newLang = currentLang === "es" ? "en" : "es";
+  document.documentElement.lang = newLang;
+
+  for (const [id, texts] of Object.entries(translations)) {
+    const element = document.getElementById(id);
+
+    if (element) {
+      if (element.querySelector(".icon")) {
+        element.querySelector(
+          ".icon"
+        ).nextSibling.nodeValue = ` ${texts[newLang]}`;
+      } else {
+        element.innerHTML = texts[newLang];
       }
     }
-  
-    document.getElementById("flag-icon").src =
-      newLang === "es"
-        ? "https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg"
-        : "https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Spain.svg";
   }
-  
+
+  document.getElementById("flag-icon").src =
+    newLang === "es"
+      ? "https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg"
+      : "https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Spain.svg";
+}
